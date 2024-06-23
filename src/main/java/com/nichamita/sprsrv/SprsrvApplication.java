ce68e5a5350d363aabd4f08159cb16275e3a001a
Jason Gronn <jasontopia03@gmail.com>
2024-06-23 19:37:03 -0400

    Initial Commit

---
+++ src/main/java/com/nichamita/sprsrv/SprsrvApplication.java
@@ -0,0 +1,13 @@
+package com.nichamita.sprsrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprsrvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprsrvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nichamita/sprsrv/SprsrvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nichamita.sprsrv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprsrvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
